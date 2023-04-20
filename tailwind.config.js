09243055c4dc81d646ff1ade863ec94545b34bad
lyjeileen <lyjeileen@gmail.com>
2023-04-20 08:40:17 -0700

    Set up theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      softred: '#EFAC91',
+      brown: '#C27947',
+      lightgreen: '#AFBBA2',
+      base: '#F8EDE6',
+      grassgreen: '#8E9D7D',
+    },
     extend: {},
   },
   plugins: [],
