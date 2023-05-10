e76ade01b9c9e0e33145bf6d0a7c365ab6c602f2
lyjeileen <lyjeileen@gmail.com>
2023-05-10 00:08:27 -0700

    Change logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,10 @@
-import { RiUserStarLine } from 'react-icons/ri';
+import { BsHouseHeart } from 'react-icons/bs';
 export default function Navbar() {
   return (
     <nav className="bg-lightgreen px-2 sm:px-4 py-2.5 fixed top-0 left-0 w-screen z-50">
       <div className="flex flex-wrap items-center justify-between text-base">
         <div className="flex items-center">
-          <RiUserStarLine className="fill-base w-8 h-8" />
+          <BsHouseHeart className="fill-base w-7 h-7" />
           <span className="self-center m-2 text-xl text-amber-800 font-bold whitespace-nowrap">
             Eileen Li
           </span>
