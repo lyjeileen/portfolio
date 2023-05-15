6b2eddc1a0ef9c3167d0122ad93e3d787f2e1a70
lyjeileen <lyjeileen@gmail.com>
2023-05-15 13:22:43 -0700

    Change css style for contact button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import { BsHouseHeart } from 'react-icons/bs';
 
 export default function Navbar() {
   return (
-    <nav className="bg-lightgreen fixed top-0 left-0 w-screen z-50">
+    <nav className="bg-lightgreen fixed top-0 left-0 w-full z-50">
       <div className="flex flex-wrap items-center justify-between px-4 py-2.5 text-base font-bold">
         <div className="flex items-center">
           <BsHouseHeart className="fill-base w-7 h-7" />
@@ -13,7 +13,9 @@ export default function Navbar() {
           </span>
         </div>
         <Link href="mailto:yuejiao.li.eileen@gmail.com">
-          <p className="mr-4 border-2 px-2 rounded-3xl text-lg">Contact</p>
+          <p className="border-2 px-2 rounded-3xl text-lg text-base hover:text-lightgreen hover:bg-base">
+            Contact
+          </p>
         </Link>
       </div>
     </nav>
