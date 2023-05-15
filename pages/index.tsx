6d90f9b639332bdb16e25488c9fcf3d122b4a353
lyjeileen <lyjeileen@gmail.com>
2023-05-15 13:32:06 -0700

    Make intro part into a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,12 @@
-import Image from 'next/image';
 import Projects from 'components/Projects';
 import Contact from 'components/Contact';
 import Skills from '@/components/Skills';
+import Intro from '@/components/Intro';
 
 export default function Home() {
   return (
     <div className="pt-20">
-      <div className="flex flex-col text-center md:m-10">
-        <div className="flex justify-center">
-          <Image src="/flower.svg" alt="flower" width={40} height={40} />
-          <h1 className="text-grassgreen text-3xl font-bold mx-4 text-center md:text-5xl lg:text-6xl ">
-            Hi, I&apos;m Eileen Li.
-          </h1>
-          <Image src="/flower.svg" alt="flower" width={40} height={40} />
-        </div>
-        <div className="text-softred my-8">
-          <p className="text-center text-md md:text-xl p-2 min-[1300px]:hidden">
-            Welcome to my portfolio website! I&apos;m excited to showcase my
-            projects and skills to potential employers. Take a look around and
-            feel free to contact me with any questions or opportunities.
-          </p>
-          <div className="text-center text-2xl text-md hidden min-[1300px]:block">
-            <p>Welcome to my portfolio website!</p>
-            <p>
-              I&apos;m excited to showcase my projects and skills to potential
-              employers.
-            </p>
-            <p>
-              Take a look around and feel free to contact me with any questions
-              or opportunities.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Intro />
       <Projects />
       <Skills />
       {/* <h1 className="text-grassgreen text-4xl text-center font-bold mx-4">
